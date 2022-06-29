c0297b63a49861ddcdfcbecec14f1b9373f6efeb
RealWorldd <jinpark99@naver.com>
2022-06-30 06:22:57 +0900

    :recycle:Refactor code

--- src/controllers/testController.js
+++
@@ -1,5 +0,0 @@
-export const test = (req, res) => res.send("Join");
-export const test5 = (req, res) => res.send("Join");
-export const test6 = (req, res) => res.send("Join");
-export const test2 = (req, res) => res.send("Join");
-export const test4 = (req, res) => res.send("Join")

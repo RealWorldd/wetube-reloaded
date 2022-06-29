c12228daa71a17504910b0c181b8bc4d469ea694
RealWorldd <jinpark99@naver.com>
2022-06-30 06:11:24 +0900

    test

--- src/controllers/testController.js
+++ src/controllers/testController.js
@@ -1,2 +1,6 @@
 export const test = (req, res) => res.send("Join");
-export const test1 = (req, res) => res.send("Join");
+export const test5 = (req, res) => res.send("Join");
+export const test6 = (req, res) => res.send("Join");
+export const test2 = (req, res) => res.send("Join");
+export const test4 = (req, res) => res.send("Join");
+

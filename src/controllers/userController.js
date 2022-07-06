f040db70f06f1bd1d49fff89e52ec1684aeae6e3
RealWorldd <jinpark99@naver.com>
2022-07-07 05:45:42 +0900

    :white_check_mark: 회원가입 추가

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,7 +1,41 @@
-export const join = (req, res) => res.send("Join");
+import User from "../models/User";
+
+export const getJoin = (req, res) => {
+  res.render("join", { pageTitle: "Join" });
+};
+
+export const postJoin = async (req, res) => {
+  const { email, password, password2, username, name, location } = req.body;
+  const pageTitle = "Join";
+  if(password !== password2){
+    return res.status(400).render("join", {
+        pageTitle,
+        errorMessage: "Password confirmation does not match.",
+    })
+  }
+  const exists = await User.exists({ $or: [{ username }, { email }] });
+  if (exists) {
+    return res.status(400).render("join", {
+      pageTitle,
+      errorMessage: "This username/email is already taken.",
+    });
+  }
+  await User.create({
+    email,
+    password,
+    username,
+    name,
+    location,
+  });
+  return res.redirect("/login");
+};
+
 export const edit = (req, res) => res.send("Edit User");
+
 export const remove = (req, res) => res.send("Delete User");
+
 export const login = (req, res) => res.send("Login");
+
 export const logout = (req, res) => res.send("Log Out");
+
 export const see = (req, res) => res.send("See User");
-export const sejin = (req, res) => res.send("1");
